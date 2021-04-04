fa25a0d3bcc6039e5d5b97eeeea733122a83e362
Alexandra Chugunova <alexandrachugunova@mail.ru>
2021-04-04 13:52:29 +0300

    ver. 1

---
+++ ptp3.py
@@ -0,0 +1,21 @@
+def get_absolute_url(url, *args, **kwargs):
+    """Формируем полный URL из домена и параметров
+    :param url: домен
+    :param args: произвольное количнство позиционных параметров
+    :param kwargs: произвольное количнство именованных параметров"""
+
+
+# добавляем к домену "/позиционный агрумент" и '?' после всех позиционных аргументов
+    for i in args:
+        url +='/'+i
+    url += '?'
+# добавляем к url именованные аргументы
+    for k, v in kwargs.items():
+        url += k + '=' + v + '&'
+# убираем лишний '&' в конце строки
+    url = url [0: -1]
+
+    return url
+
+print (get_absolute_url('www.yandex.ru', 'posts','news',id='24',author='admin'))
+print (get_absolute_url('www.google.com','images',id='24',category='auto',color='red',size='small'))
